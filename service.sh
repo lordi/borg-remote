5648b93209c0f09f1a1515f3d5b53fe7ec761c1e
Hannes Gräuler <hannes@smasi.de>
2022-03-25 17:25:33 +0100

    Add pubkey to authorized_keys

--- service.sh
+++ service.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+mkdir -p ~borg/.ssh
+echo "command=\"/usr/bin/borg serve --append-only --restrict-to-path /backup\" ssh-rsa $SSH_PUB_RSA" >> ~borg/.ssh/authorized_keys
+chown borg:borg -R ~borg/.ssh
 ssh-keygen -A
-/usr/sbin/sshd -D
+/usr/sbin/sshd -D -e
